e051c2e556d0431228225d1a595356ccc393741f
hath <hath@nustechnology.com>
2018-05-01 15:52:29 +0700

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root "home#index"
+  class RequestConstraint
+    def self.matches?(request)
+      /(\.well-known)/i.match(request.original_url).present?
+    end
+  end
+
+  match "*path", to: "home#index", via: :all, constraints: RequestConstraint
 end
