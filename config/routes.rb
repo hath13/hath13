364e77672fd48a0adef7de71d754b5d7c7800064
hath <hath@nustechnology.com>
2018-05-03 22:12:20 +0700

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :motors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "home#index"
+  root "motors#index"
   class RequestConstraint
     def self.matches?(request)
       /(\.well-known)/i.match(request.original_url).present?
---
+++ test/system/motors_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class MotorsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit motors_url
+  #
+  #   assert_selector "h1", text: "Motor"
+  # end
+end
